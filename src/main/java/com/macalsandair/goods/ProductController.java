0d5f916e1be00b6c30ab82f1a4ea869bfa651e12
MacAlsandair <mac.alsandair@gmail.com>
2022-11-15 00:34:38 +0200

    Add a JpaRepository to product

--- src/main/java/com/macalsandair/goods/ProductController.java
+++ src/main/java/com/macalsandair/goods/ProductController.java
@@ -1,6 +1,8 @@
 package com.macalsandair.goods;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,9 +10,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/product")
 public class ProductController {
 	
-	@GetMapping("/")
-	public Product getProduct () {
+	@GetMapping("/{id}")
+	public Product getProduct(@PathVariable("id") Long id ) {
 		
+		return null;
 	}
+	
+	@PostMapping()
+	public Product createProduct(@PathVariable("id") Long id ) {
+		
+		return null;
+	}
+	
+	@PostMapping() 
+		
+	
+	
+	
 
 }
---
+++ src/main/java/com/macalsandair/goods/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.macalsandair.goods;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
