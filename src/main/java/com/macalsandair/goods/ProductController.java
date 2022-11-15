495a77e46721295dc475327a0de7277720fa3f5f
MacAlsandair <mac.alsandair@gmail.com>
2022-11-15 12:24:16 +0200

    Create a simple billet of a product API

--- src/main/java/com/macalsandair/goods/ProductController.java
+++ src/main/java/com/macalsandair/goods/ProductController.java
@@ -1,8 +1,13 @@
 package com.macalsandair.goods;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,13 +22,30 @@ public class ProductController {
 	}
 	
 	@PostMapping()
-	public Product createProduct(@PathVariable("id") Long id ) {
+	public ResponseEntity<String> createProduct(@PathVariable("id") Long id ) {
 		
-		return null;
+		return new ResponseEntity<String>(HttpStatus.CREATED);
 	}
 	
-	@PostMapping() 
+	@PutMapping("/{id}")
+	public ResponseEntity<String> changeTheWholeProduct (@PathVariable("id") Long id) {
 		
+		return new ResponseEntity<String> (HttpStatus.OK);
+	}
+	
+	@PatchMapping("/{id}")
+	public ResponseEntity<String> changeThePartOfAProduct (@PathVariable("id") Long id) {
+		
+		return new ResponseEntity<String> (HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<String> deleteProduct (@PathVariable("id") Long id) {
+		
+		return new ResponseEntity<String> (HttpStatus.OK);
+	}
+	
+	
 	
 	
 	
