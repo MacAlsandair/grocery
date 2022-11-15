f369334d500167507c3cf366d729b2b61102307c
MacAlsandair <mac.alsandair@gmail.com>
2022-11-15 12:45:59 +0200

    Correct the url of an order controller

--- src/main/java/com/macalsandair/order/OrderController.java
+++ src/main/java/com/macalsandair/order/OrderController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping("/api/product")
+@RequestMapping("/api/order")
 public class OrderController {
 	
 	@GetMapping
