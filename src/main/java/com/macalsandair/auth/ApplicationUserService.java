124d60edb399f29a1fd66a1be94839678b725940
MacAlsandair <mac.alsandair@gmail.com>
2022-11-15 16:37:03 +0200

    Add an ApplicationUserService inner logic

--- src/main/java/com/macalsandair/auth/ApplicationUserService.java
+++ src/main/java/com/macalsandair/auth/ApplicationUserService.java
@@ -1,15 +1,25 @@
 package com.macalsandair.auth;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 public class ApplicationUserService implements UserDetailsService {
 
+	private final ApplicationUserDao applicationUserDao;
+	
+	public ApplicationUserService(@Qualifier("postgresUserDao") ApplicationUserDao applicationUserDao) {
+		super();
+		this.applicationUserDao = applicationUserDao;
+	}
+
+
+
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
+		return applicationUserDao.selectApplicationUserByUsername(username).orElseThrow(
+				() -> new UsernameNotFoundException(String.format("Username %s not found", username)));
 	}
 
 }
