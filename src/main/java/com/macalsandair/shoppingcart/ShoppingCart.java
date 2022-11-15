010f490aa8fff1ca7f8ce603826b2ae8b87daa06
MacAlsandair <mac.alsandair@gmail.com>
2022-11-15 13:09:06 +0200

    Add  a shopping cart TODO

---
+++ src/main/java/com/macalsandair/shoppingcart/ShoppingCart.java
@@ -0,0 +1,8 @@
+package com.macalsandair.shoppingcart;
+
+
+//TODO create a logic, how can i spare a shopping cart for a simple and anonymous user
+//Now i will only create a pretty simple logic with only a cart on a client side
+public class ShoppingCart {
+
+}
