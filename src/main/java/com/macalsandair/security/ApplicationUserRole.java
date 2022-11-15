4672e1179652d247241f62cc15539f406d04cd2c
MacAlsandair <mac.alsandair@gmail.com>
2022-11-15 16:29:01 +0200

    Add an aplication user class

--- src/main/java/com/macalsandair/security/ApplicationUserRole.java
+++ src/main/java/com/macalsandair/security/ApplicationUserRole.java
@@ -1,6 +1,18 @@
 package com.macalsandair.security;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
 public enum ApplicationUserRole {
 	CUSTOMER,
-	ADMIN
+	ADMIN;
+	
+	public Set <SimpleGrantedAuthority> getGrantedAuthorities() {
+		Set<SimpleGrantedAuthority> permissions = new HashSet<>();;
+		permissions.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
+		return permissions;
+	}
 }
